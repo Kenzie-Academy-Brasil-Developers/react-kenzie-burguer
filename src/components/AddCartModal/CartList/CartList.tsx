640eab9ca838663a70aea1bd09338fe68d1f0ff1
Gabriel <gabpavao@outlook.com>
2022-12-20 16:18:54 -0300

    feat: cart list HTML

--- src/components/AddCartModal/CartList/CartList.tsx
+++ src/components/AddCartModal/CartList/CartList.tsx
@@ -1,10 +1,17 @@
+import { StyledTitle } from "../../../styles/Typography";
 import { CartCard } from "./CartCard";
 
 
 export function CartList () {
     return (
-        <ul>
-            <CartCard/>
-        </ul>
+        <>
+            <ul>
+                <CartCard/>
+            </ul>
+
+            <StyledTitle tag="h4">Total</StyledTitle>
+            <small>R$ 14,00</small>
+            <button type="button">Remover todos</button>       
+        </>
     )
 }
