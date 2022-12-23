94c5a8da00653edd384450e8f59775f6de558d35
Gabriel <gabpavao@outlook.com>
2022-12-23 09:56:29 -0300

    feat: menu page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,8 @@ import { StyledContainerHeader } from "../../styles/Container";
 import { StyledTitle } from "../../styles/Typography";
 import { SearchInput } from "./SearchInput";
 import { StyledHeader } from "./StyledHeader";
+import searchIcon from "../../assets/searchIcon.svg"
+import searchIconInactive from "../../assets/searchIconInactive.svg"
 import cartIcon from "../../assets/cartIcon.svg"
 import exitIcon from "../../assets/exitIcon.svg"
 import { useContext } from "react";
@@ -9,7 +11,7 @@ import { UserContext } from "../../contexts/UserContext";
 import { CartContext } from "../../contexts/CartContext";
 
 export function Header () {
-    const { navigate, productsCartList } = useContext(UserContext)
+    const { isSearchActivated, setIsSearchActivated, navigate, productsCartList } = useContext(UserContext)
     const { setIsCartModalOpen } = useContext(CartContext)
     
     function logout () {
@@ -23,7 +25,8 @@ export function Header () {
                 <StyledTitle tag="h1">Burguer <span>Kenzie</span></StyledTitle>
 
                 <div>
-                    <SearchInput/>
+                    {isSearchActivated && <SearchInput/>}
+                    <img onClick={() => {setIsSearchActivated(true)}} src={searchIconInactive} alt="" />
                     <img onClick={() => {setIsCartModalOpen(true)}} src={cartIcon} alt="Carrinho de Compras" />
                     <span className="counter">{productsCartList.length}</span>
                     <img onClick={logout} src={exitIcon} alt="Encerrar sessão" />
